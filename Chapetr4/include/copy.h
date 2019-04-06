11b800538bd8bcb71e5d1753b1a9a4a658584b0a
Title0A <49059607+Title0A@users.noreply.github.com>
2019-04-06 20:54:50 +0800

    各个函数对应的头文件

---
+++ Chapetr4/include/copy.h
@@ -0,0 +1,9 @@
+#ifndef _COPY_H
+#define _COPY_H
+#define d_nu   50
+#define s_nu   25
+
+void copy_n(char const dst[],char src[],int n);
+
+
+#endif
---
+++ Chapetr4/include/prime.h
@@ -0,0 +1,4 @@
+#ifndef PRIME_H
+#define PRIME_H
+void prime(int MAX);
+#endif
